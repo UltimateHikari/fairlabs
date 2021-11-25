e99742d4ed8a1e7bc75e5714a0cc2f539030f310
UltimateHikari <andy@andystpad.localdomain>
2021-11-25 22:35:01 +0700

    converters for admin

--- server/adapter/conv/admin.go
+++ server/adapter/conv/admin.go
@@ -3,42 +3,78 @@ package conv
 import "fairlabs-server/logic/spec"
 
 type AdminAddRequest struct {
-	Context UserContext
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
+	UserContext
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type AdminAlgo struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 type AdminAlgoPostRequest struct {
-	Context UserContext
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
+	UserContext
+	AdminAlgo
+}
+
+type AdminCondition struct {
+	Id   int      `json:"id"`
+	Name string   `json:"name"`
+	Data []string `json:"data"`
 }
 
 type AdminConditionPostRequest struct {
-	Context UserContext
-	Id      int      `json:"id"`
-	Name    string   `json:"name"`
-	Data    []string `json:"data"`
+	UserContext
+	AdminCondition
 }
 
-func (r *AdminAddRequest) toCourseInfo() *spec.CourseInfo {
+func (r *AdminAddRequest) ToCourseInfo() *spec.CourseInfo {
 	var courseInfo spec.CourseInfo
 	courseInfo.Id = r.Id
 	courseInfo.Name = r.Name
 	return &courseInfo
 }
 
-func (r *AdminAlgoPostRequest) toAlgo() *spec.Algo {
+func (r *AdminAlgoPostRequest) ToAlgo() *spec.Algo {
 	var algo spec.Algo
 	algo.Id = r.Id
 	algo.Name = r.Name
 	return &algo
 }
 
-func (r *AdminConditionPostRequest) toCondition() *spec.Condition {
+func (r *AdminConditionPostRequest) ToCondition() *spec.Condition {
 	var condition spec.Condition
 	condition.Id = r.Id
 	condition.Name = r.Name
 	condition.Data = r.Data
 	return &condition
+}
+
+type AdminAlgoGetResponse struct {
+	Algos []AdminAlgo `json:"algos"`
+}
+
+type AdminConditionGetResponse struct {
+	Conds []AdminCondition `json:"conditions"`
+}
+
+func ToAlgoResponse(algos []spec.Algo) *AdminAlgoGetResponse {
+	var response AdminAlgoGetResponse
+	response.Algos = make([]AdminAlgo, len(algos))
+	for i, item := range algos {
+		response.Algos[i].Id = item.Id
+		response.Algos[i].Name = item.Name
+	}
+	return &response
+}
+
+func ToConditionResponse(conditions []spec.Condition) *AdminConditionGetResponse {
+	var response AdminConditionGetResponse
+	response.Conds = make([]AdminCondition, len(conditions))
+	for i, item := range conditions {
+		response.Conds[i].Id = item.Id
+		response.Conds[i].Name = item.Name
+	}
+	return &response
 }
--- server/adapter/conv/conv.go
+++ server/adapter/conv/conv.go
@@ -2,7 +2,7 @@ package conv
 
 // checks for admin or not are in logic
 type UserContext struct {
-	Email string `json:"email"`
-	Name  string `json:"name"`
-	Group string `json:"group"`
+	UserEmail string `json:"email"`
+	UserName  string `json:"username"`
+	UserGroup string `json:"group"`
 }
