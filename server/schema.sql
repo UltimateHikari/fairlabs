b0b0fc40bd584a43a3496e42246f24f98c92ca4c
UltimateHikari <andy@andystpad.localdomain>
2021-11-28 10:05:04 +0700

    schema for postgres

---
+++ server/schema.sql
@@ -0,0 +1,69 @@
+DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS algos;
+DROP TABLE IF EXISTS conds;
+
+CREATE TABLE algos(
+    algo_id integer PRIMARY KEY,
+    algo_name text NOT NULL
+);
+
+CREATE TABLE conds(
+    cond_id integer PRIMARY KEY,
+    cond_name text NOT NULL,
+    example_data text[]
+);
+
+--todo add tasks count
+CREATE TABLE courses(
+    course_id integer PRIMARY KEY,
+    course_name text NOT NULL,
+    university_group integer NOT NULL,
+    algo integer REFERENCES algos (algo_id),
+    cond_id integer REFERENCES conds (cond_id)
+);
+
+CREATE TABLE users(
+    user_id integer PRIMARY KEY,
+    email text NOT NULL,
+    user_name text NOT NULL
+);
+
+--role == STUDENT/TEACHER
+CREATE TABLE groups(
+    group_number integer,
+    user_number integer REFERENCES users (user_id),
+    user_role text,
+    PRIMARY KEY(group_number, user_number)
+);
+
+INSERT INTO algos (algo_id, algo_name) 
+    VALUES(1, 'alphabet-descending');
+INSERT INTO algos (algo_id, algo_name) 
+    VALUES(2, 'datetime-filo');
+INSERT INTO algos (algo_id, algo_name) 
+    VALUES(3, 'persentage-ascending');
+
+INSERT INTO conds (cond_id, cond_name, example_data) 
+    VALUES(1, 'deadlines', 
+    '{"21.09:1", "15.10:2", "29.10:3", "27.11:4", "22.11:5"}');
+INSERT INTO conds (cond_id, cond_name, example_data) 
+    VALUES(2, 'task_amounts', 
+    '{"6:3", "11:4", "23:5"}');
+
+INSERT INTO users (user_id, email, user_name)
+    VALUES (1, 'a.rudometov@g.nsu.ru', 'Andrey Rudometov');
+INSERT INTO users (user_id, email, user_name)
+    VALUES (2, 'a.ogneva@g.nsu.ru', 'Anastasia Ogneva');
+
+INSERT INTO groups (group_number, user_number, user_role)
+    VALUES (19201, 1, 'STUDENT');
+INSERT INTO groups (group_number, user_number, user_role)
+    VALUES (19201, 2, 'STUDENT');
+
+
+
+
+
+
