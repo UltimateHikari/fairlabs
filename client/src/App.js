a9fb0d7d2409f17c20f04a85d04f1f62382271c4
kihoii <kihikolee@gmail.com>
2021-11-29 18:45:15 +0700

    client-initial

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import About from "./pages/About";
+
+function App() {
+  return (
+      <BrowserRouter>
+          <Routes>
+              <Route path="/about" element={<About/>}/>
+          </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
