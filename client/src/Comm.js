2c50dff119ad3492ec72ac313fd34a49b737b6c6
UltimateHikari <andy@andystpad.localdomain>
2021-12-02 18:35:34 +0700

    cors proxy example

--- client/src/Comm.js
+++ client/src/Comm.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class Comm{
     static async getSth(){
-        const response = await axios.get('http://localhost:8080/v1/admin/get/algo')
+        const response = await axios.get('http://localhost:3000/v1/admin/get/algo')
 
         return response
     }
